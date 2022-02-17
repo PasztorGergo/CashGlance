6865625f842e56303ad6c648d45495bad6c9907a
PasztorGergo <pasztorg05@gmail.com>
2022-02-17 18:16:01 +0100

    Chain animation 🔗

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -3,107 +3,91 @@ import styles from "../styles/Navigation.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 import { useFirebase } from "../hooks/FirebaseContext";
-import { Transition, animated } from "@react-spring/web";
+import Image from "next/image";
+import {
+  Transition,
+  animated,
+  useChain,
+  useSpringRef,
+  useSpring,
+  config,
+  useTransition,
+} from "@react-spring/web";
 
 type Props = {};
 
 export default function Navigation({}: Props) {
   const { currentUser } = useFirebase();
   const [open, setOpen] = useState(false);
+  const springApi = useSpringRef();
+  const { opacity, ...rest } = useSpring({
+    ref: springApi,
+    config: config.stiff,
+    from: { opacity: 0 },
+    to: {
+      opacity: open ? 1 : 0,
+    },
+  });
+  const transApi = useSpringRef();
+  const transition = useTransition(
+    open
+      ? [
+          { icon: ["fas", "chart-line"], link: "charts" },
+          { icon: ["fas", "money-check"], link: "statement" },
+          { icon: ["fas", "file-invoice-dollar"], link: "portfolio" },
+          { icon: currentUser?.photoURL ?? ["fas", "user"], link: "account" },
+        ]
+      : [],
+    {
+      trail: 100,
+      ref: transApi,
+      enter: { opacity: 1, scale: 1 },
+      leave: { opacity: 0, scale: 0 },
+      from: { opacity: 0, scale: 0 },
+      to: { opacity: 1, scale: 1 },
+    }
+  );
+  useChain(open ? [springApi, transApi] : [transApi, springApi]);
 
-  return (
-    <Transition
-      items={open}
-      from={{ opacity: 0 }}
-      to={{ opacity: 1 }}
-      leave={{ opacity: 0 }}
-      enter={{ opacity: 1 }}
-      reverse={open}
+  return open ? (
+    <animated.nav
+      className={`${styles.navigation} prose-h2:text-white`}
+      style={{ opacity: opacity.to({ range: [0, 1], output: [0, 1] }) }}
     >
-      {({ opacity }, item) =>
-        item ? (
-          <animated.nav
-            className={`${styles.navigation} prose-h2:text-white`}
-            style={{ opacity: opacity.to({ range: [0, 1], output: [0, 1] }) }}
-          >
-            <Link href="/">
-              <h2 className={styles.brand}>CashGlance</h2>
+      <Link href="/">
+        <h2 className={styles.brand}>CashGlance</h2>
+      </Link>
+      <ul className={styles.navList}>
+        {transition((style, item) => (
+          <animated.li style={{ ...style }}>
+            <Link href={item.link}>
+              <div className={`${styles.icon} group`}>
+                {typeof item.icon == "string" ? (
+                  <Image src={item.icon} />
+                ) : (
+                  <FontAwesomeIcon icon={item.icon} />
+                )}
+                <p
+                  className={`${styles.iconText} group-hover:min-h-max group-hover:opacity-100`}
+                >
+                  {item.link}
+                </p>
+              </div>
             </Link>
-            <ul className={styles.navList}>
-              <li>
-                <Link href="/charts">
-                  <div className={`${styles.icon} group`}>
-                    <FontAwesomeIcon icon={["fas", "chart-bar"]} />
-                    <p
-                      className={`${styles.iconText} group-hover:w-full group-hover:min-h-max group-hover:opacity-100`}
-                    >
-                      Charts
-                    </p>
-                  </div>
-                </Link>
-              </li>
-              <li>
-                <Link href="statement">
-                  <div className={`${styles.icon} group`}>
-                    <FontAwesomeIcon icon={["fas", "file-invoice"]} />
-                    <p
-                      className={`${styles.iconText} group-hover:min-h-max group-hover:opacity-100`}
-                    >
-                      Statement
-                    </p>
-                  </div>
-                </Link>
-              </li>
-              <li>
-                <Link href="/portfolio">
-                  <div className={`${styles.icon} group`}>
-                    <FontAwesomeIcon icon={["fas", "info-circle"]} />
-                    <p
-                      className={`${styles.iconText} group-hover:w-full group-hover:min-h-max group-hover:opacity-100`}
-                    >
-                      Portfolio
-                    </p>
-                  </div>
-                </Link>
-              </li>
-              <li>
-                <Link href={currentUser ? "/account" : "/signin"}>
-                  <div className={`${styles.icon} group`}>
-                    {currentUser?.photoURL ? (
-                      <div
-                        className={styles.avatar}
-                        style={{ backgroundImage: currentUser.photoURL }}
-                      ></div>
-                    ) : (
-                      <FontAwesomeIcon icon={["fas", "user-circle"]} />
-                    )}
-                    <p
-                      className={`${styles.iconText} group-hover:w-full group-hover:min-h-max group-hover:opacity-100`}
-                    >
-                      {currentUser ? currentUser.displayName : "Sign In"}
-                    </p>
-                  </div>
-                </Link>
-              </li>
-              <li>
-                <FontAwesomeIcon
-                  icon={["fas", "times"]}
-                  onClick={() => setOpen(false)}
-                  className="cursor-pointer"
-                />
-              </li>
-            </ul>
-          </animated.nav>
-        ) : (
-          <animated.nav
-            onClick={() => setOpen(true)}
-            className="rounded-full h-16 w-16 text-3xl cursor-pointer flex justify-center items-center fixed bg-gradient-to-br top-4 left-4 z-10 from-emerald-900 to-emerald-800 text-white"
-            style={{ opacity: opacity.to({ range: [0, 1], output: [0, 1] }) }}
-          >
-            <FontAwesomeIcon icon={["fas", "bars"]} />
-          </animated.nav>
-        )
-      }
-    </Transition>
+          </animated.li>
+        ))}
+        <li onClick={() => setOpen(false)}>
+          <FontAwesomeIcon icon={["fas", "times"]} />
+        </li>
+      </ul>
+    </animated.nav>
+  ) : (
+    <animated.nav
+      onClick={() => setOpen(true)}
+      className="rounded-full h-16 w-16 text-3xl cursor-pointer flex justify-center items-center fixed bg-gradient-to-br top-4 left-4 z-10 from-emerald-900 to-emerald-800 text-white"
+      style={{ opacity: opacity.to({ range: [0, 1], output: [0, 1] }) }}
+    >
+      <FontAwesomeIcon icon={["fas", "bars"]} />
+    </animated.nav>
   );
 }
